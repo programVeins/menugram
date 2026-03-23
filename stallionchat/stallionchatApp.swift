325d0ff87262bca70576f398b910d40cb0b27f06
Sabesh <sabesh@consciousengines.com>
2026-03-23 14:29:27 +0530

    Initial Commit

---
+++ stallionchat/stallionchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  stallionchatApp.swift
+//  stallionchat
+//
+//  Created by Sabesh Bharathi on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct stallionchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
